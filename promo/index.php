1074ebfb3fb95805cdd2f4c1fca216d4a9be4842
PavelRoR <pavelvp2182@yandex.ru>
2022-03-30 16:55:21 +0500

    Logo and Promo edits

--- promo/index.php
+++ promo/index.php
@@ -29,7 +29,7 @@
     <div class="wrapper">
       <header>
         <div class="container"><a href="https://mastervision.su" target="_blank"><img class="header-logo" src="../img/images/header-logo.png" alt="Логотип"/></a>
-          <div class="contacts"><a class="contact contact-tg" href="https://t.me/Master_Vision_Academy" target="_blank"><img src="../img/icons/tg.png" alt="Telegram"/></a><a class="contact contact-email" href="mailto:info@mastervision.su" target="_blank"><img src="../img/icons/envelope.png" alt="Email"/></a></div>
+          <div class="contacts"><a class="contact contact-tg" href="https://t.me/Master_Vision_Academy" target="_blank"><img src="../img/icons/tg.png" alt="Telegram"/></a><a class="contact contact-email" href="mailto:info@mv-centr.ru" target="_blank"><img src="../img/icons/envelope.png" alt="Email"/></a></div>
         </div>
       </header>
       <section id="main">
@@ -39,17 +39,11 @@
             <div class="circle circle-bordered circle-2"></div>
             <div class="circle circle-full circle-3"></div>
             <div class="row">
-              <div class="col-md-5">
-                <div class="video-wrapper video-wrapper-main">
-                  <iframe src="https://www.youtube.com/embed/c33eMwoyroo?autoplay=1&amp;mute=1&amp;modestbranding=1" allowfullscreen></iframe>
-                </div>
-              </div>
-              <div class="col-md-7">
-                <div class="content">
-                  <p class="main-text"><span>У нас к Вам просьба прямо сейчас подписаться на Телеграм.</span><br>В нем Вы будете получать и напоминания о начале мастер-класса с ссылками на вебинарную комнату</p>
-                  <p class="main-text">В благодарность за регистрацию на мастер-класс, в Телеграме Вы получите приятный и полезный бонус - <span>видео-урок "Дизайн человека за 5 минут"</span></p>
-                  <h3 class="main-subtitle"><span>Перейдите в Телеграм</span></h3><a class="button button-main" href="https://shop.mv-centr.ru/index.php?r=form/telegram&amp;id_bot=6">Подписаться <img src="../img/icons/tg.png" alt="Telegram"></a>
-                </div>
+              <div class="col-md-8">
+                <p class="main-text"><span>Спасибо, что Вы с нами.</span><br>Если Вы еще не подписались на наш Telegram-канал, то сделайте это прямо сейчас по кнопке ниже</p>
+                <p class="main-text">В Telegram мы сразу пришлем ссылку на <span>вводный урок по "Дизайн человека"</span></p>
+                <p class="main-text">Рекомендуем посмотреть его до начала мастер-класса, Вам важно будет подготовиться и прийти на мастер-класс с расчитанной картой, пониманием базовых терминов и понятий из "Дизайна человека".</p>
+                <h3 class="main-subtitle"><span>Перейдите в Телеграм</span></h3><a class="button button-main" href="https://shop.mv-centr.ru/index.php?r=form/telegram&amp;id_bot=6">Подписаться <img src="../img/icons/tg.png" alt="Telegram"></a>
               </div>
             </div>
           </div>
@@ -60,13 +54,13 @@
       <div class="container"><a href="https:/mastervision.su/" target="_blank"><img class="footer-logo" src="../img/images/footer-logo.png" alt="Логотип"/></a>
         <div class="row">
           <div class="col-md-6">
-            <p class="footer-text">Тюмень, проезд Летний, 9а</p>
+            <p class="footer-text">Ростовская обл., г.Волгодонск ул.Ленинградская, 19/71 кв.219</p>
             <p class="footer-text"><a href="tel:8-4993467946">8-499-346-79-46</a></p>
-            <p class="footer-text"><a href="mailto:info@mastervision.su">info@mastervision.su</a></p>
+            <p class="footer-text"><a href="mailto:info@mv-centr.ru">info@mv-centr.ru</a></p>
           </div>
           <div class="col-md-6">
-            <p class="footer-text footer-text-right">ИП Кудряшов А. А.</p>
-            <p class="footer-text footer-text-right">ИНН 720410077658</p>
+            <p class="footer-text footer-text-right">ИП Кудряшова А. А.</p>
+            <p class="footer-text footer-text-right">ИНН/КПП: 614311503399</p>
           </div>
         </div>
         <hr class="footer-hr"><a class="footer-docs" href="https://mastervision.su/politics.pdf" download target="_blank">Политика конфиденциальности.</a><a class="footer-docs" href="https://mastervision.su/oferta.docx" download target="_blank">Публичная оферта.</a>
